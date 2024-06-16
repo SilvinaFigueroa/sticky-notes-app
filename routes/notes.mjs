07e81d907449354b5f2897a49f2b83a3c02e0de8
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-06-15 19:10:35 -0700

    include uuid to generate unique ID for notes

--- routes/notes.mjs
+++ routes/notes.mjs
@@ -3,14 +3,31 @@ import months from '../data/months.mjs'
 import users from '../data/users.mjs'
 import notes from '../data/notes.mjs'
 
+// unique id generator
+import { v4 as uuidv4 } from 'uuid'
 
 
 const router = express.Router()
 
 
 router.get("/add", (req,res)=>{
-    res.render('form')
+    res.render('notes')
 })
 
+router.post("/add", (req,res)=>{
+    const userEmail = req.query
+    
+    notes.push({
+        // {
+        //     id: 1,
+        //     userId: 1,
+        //     day: 1,
+        //     monthId: 6,
+        //     content: 'Start of the month planning'
+        // },
+
+    })
+
+})
 
 export default router
